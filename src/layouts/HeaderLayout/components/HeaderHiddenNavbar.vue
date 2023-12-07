a8a059b10b67dee194d5db2820337636bbeea3a9
Muslim-571 <turabovumid587@gmail.com>
2023-12-07 16:42:47 +0500

    cool

--- src/layouts/HeaderLayout/components/HeaderHiddenNavbar.vue
+++ src/layouts/HeaderLayout/components/HeaderHiddenNavbar.vue
@@ -40,7 +40,7 @@
           </i>
         </li>
         <div
-          class="px-6 py-2 md:px-6 md:py-4 border-[1px] Interfaces rounded-[88px] bg-white dark:bg-tailwind-dark flex flex-row justify-between items-center"
+          class="px-6 py-2 md:px-6 md:py-4 border-[1px] Interfaces rounded-[88px] bg-white dark:bg-tailwind-dark flex flex-row justify-between items-center w-full"
         >
           <TheDarkMode class="w-full" />
           <i class="fa-solid dark:text-white fa-arrow-right-long fa-beat"></i>
