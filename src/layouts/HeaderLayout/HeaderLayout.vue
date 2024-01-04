07ddaced044b13a269a0495a93f36b1649f1f222
Muslim-571 <turabovumid587@gmail.com>
2024-01-04 18:26:33 +0500

    Final:Final commit of this project (01.04.2024/18:27)

--- src/layouts/HeaderLayout/HeaderLayout.vue
+++ src/layouts/HeaderLayout/HeaderLayout.vue
@@ -1,5 +1,8 @@
 <template>
-  <header class="header z-[1111] w-full bg-white">
+  <header
+    class="header z-[1111] w-full bg-white"
+    :class="isScrolled ? 'header-scrolled lg:pt-0' : 'lg:pt-0'"
+  >
     <div
       class="container header__containe flex flex-row justify-between items-center py-[16px] md:flex md:flex-row md:justify-between md:items-center md:py-[14px] lg:flex lg:flex-row lg:justify-between lg:items-center lg:py-[25px]"
     >
@@ -23,6 +26,7 @@ export default {
   data() {
     return {
       isOpen: false,
+      isScrolled: false,
     };
   },
   methods: {
@@ -34,6 +38,15 @@ export default {
       this.isOpen = false;
       document.body.style.overflow = "auto";
     },
+  },
+  mounted() {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY) {
+        this.isScrolled = true;
+      } else {
+        this.isScrolled = false;
+      }
+    });
   },
   components: { HeaderNavbar, HeaderButton, HeaderHiddenNavbar },
 };
@@ -53,6 +66,10 @@ export default {
 @media (max-width: 1023px) {
   .header {
     position: fixed;
+  }
+  .header-scrolled {
+    background: rgba(255, 255, 255, 0.7);
+    backdrop-filter: blur(5px);
   }
 }
 </style>
